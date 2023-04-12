4c576444d1a6d17d82db9c6a677f1726d40ca3ab
mohammedgadeen <m.abdallah@hectar.io>
2023-04-12 11:55:41 +0200

    first commit

---
+++ Boats/Dtos/AddBoat.dto.cs
@@ -0,0 +1,10 @@
+
+namespace central_fish_agency_dotnet.Boats.Dtos
+{
+    public class AddBoatRequestDto
+    {
+        public string BoatName { get; set; } = "Boats Number 1";
+
+        public string BoatNumber { get; set; } = "BAT0001-42023";
+    }
+}
---
+++ Boats/Model/Boats.model.cs
@@ -0,0 +1,10 @@
+namespace central_fish_agency_dotnet.Boats.Model
+{
+    public class BoatsModel
+    {
+        public int Id { get; set; }
+        public string BoatName { get; set; } = "Boats Number 1";
+
+        public string BoatNumber { get; set; } = "BAT0001-42023";
+    }
+}
