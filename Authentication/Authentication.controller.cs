3aa9573317b1e32808b5645932af6c2250929974
Gadeenz <mohammedgadeen@gmail.com>
2023-04-14 17:31:21 +0200

    login implementations

--- Authentication/Authentication.controller.cs
+++ Authentication/Authentication.controller.cs
@@ -35,5 +35,16 @@ namespace central_fish_agency_dotnet.Authentication
 
             return Ok(response);
         }
+
+        [HttpPost("Login")]
+        public async Task<ActionResult<ServiceResponse<int>>> Login(UserLoginDto request)
+        {
+            var response = await _authRepo.Login(request.Email, request.Password);
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
     }
 }
